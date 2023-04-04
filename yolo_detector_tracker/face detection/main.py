61249c97fbbb84d522f181b2264ece077c4ccf95
Chee Kean CHAN <T9810610@tts.hk.thales>
2023-04-04 16:50:34 +0800

    moved .py

--- yolo_detector_tracker/face detection/main.py
+++ yolo_detector_tracker/face detection/main.py
@@ -1,16 +1,16 @@
 import os
 import cv2
-import numpy as np
 import torch
 from facenet_pytorch import InceptionResnetV1, MTCNN
 from types import MethodType
+import time
+from tqdm import tqdm
 
 # load model
-face_detector = cv2.CascadeClassifier('./weights/haarcascade_frontalface_default.xml')
 resnet = InceptionResnetV1(pretrained='vggface2').eval()
 mtcnn = MTCNN(image_size=128, keep_all=True, thresholds=[0.4, 0.5, 0.5], min_face_size=60)
 # get encoded features for all saved images
-people_pictures = "../data/saved"
+people_pictures = "./data/saved"
 all_people_faces = {}
 
 # helper function
@@ -34,19 +34,29 @@ def detect_box(self, img, save_path=None):
 mtcnn.detect_box = MethodType(detect_box, mtcnn)
 
 # feature extraction for all saved images 
-for file in os.listdir(people_pictures):
-    person_face, extension = file.split(".")
-    img = cv2.imread(f'{people_pictures}/{person_face}.jpg')
-    cropped = mtcnn(img)
-    all_people_faces[person_face] = encode(cropped)[0, :]
+start = time.time()
+for file in tqdm(os.listdir(people_pictures)):
 
-def detect(cam=0, ):
+    try:
+        person_face, extension = file.split(".")
+        img = cv2.imread(f'{people_pictures}/{person_face}.jpg')
+        cropped = mtcnn(img)
+        if cropped is not None:
+            all_people_faces[person_face] = encode(cropped)[0, :]
+    except Exception as e:
+        print(f"Failed: {file} with Error: {e}")
+
+print("Time Elapsed for Saved Images Processing: ", time.time() - start)
+print(f"Stored Image Features: {len(all_people_faces)}")
+
+def detect(cam=0):
     vdo = cv2.VideoCapture(cam)
 
     while vdo.grab():
         _, img0 = vdo.retrieve()
         batch_boxes, cropped_images = mtcnn.detect_box(img0)
 
+        start = time.time()
         if cropped_images is not None:
             for box, cropped in zip(batch_boxes, cropped_images):
                 img_embedding = encode(cropped.unsqueeze(0))
@@ -61,6 +71,7 @@ def detect(cam=0, ):
                 x, y, x2, y2 = [int(x) for x in box]
                 cv2.rectangle(img0, (x, y), (x2, y2), (0, 0, 255), 2)
                 cv2.putText(img0, min_key, (x + 5, y + 10), cv2.FONT_HERSHEY_DUPLEX, 0.5, (255, 255, 255), 1)
+            print("Time Elapsed for Image Comparison: ", time.time() - start)
                 
         ### display
         cv2.imshow("test", img0)
@@ -69,4 +80,10 @@ def detect(cam=0, ):
             break
 
 if __name__ == "__main__":
-    detect()
+    detect(0)
+
+'''
+1000 images
+Cropped + Feature Extractor: 600s
+
+'''
