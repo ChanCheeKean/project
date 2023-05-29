87e38bdef99461d46e81c4d84e04274b671e7642
cheekean <cheekeanchan@CHANs-MacBook-Pro.local>
2023-05-29 17:34:12 +0800

    updated

--- yolo_detector_tracker/yolov5_object365/main.py
+++ yolo_detector_tracker/yolov5_object365/main.py
@@ -2,15 +2,15 @@ import cv2
 import torch
 import numpy as np
 from yolov5.models.common import DetectMultiBackend
-from yolov5.utils.plots import Annotator, colors, save_one_box
+from yolov5.utils.plots import Annotator, colors
 from yolov5.utils.general import non_max_suppression, scale_boxes
 from yolov5.utils.dataloaders import LoadImages, LoadStreams
-from yolov5.utils.augmentations import letterbox
+from yolov5.utils.torch_utils import select_device, smart_inference_mode
 
 # model config
 half = False
 img_sz = [640, 640]
-device = 'cpu'
+device = ''
 classes = list(range(80))
 conf_thres = 0.5
 iou_thres = 0.5
@@ -22,6 +22,7 @@ dnn = True
 # source
 source = './test.mp4'
 webcam = source.isnumeric() or source.endswith('.txt')
+device = device = select_device(device)
 
 # output config
 show_video = True
@@ -103,53 +104,57 @@ stride, names, pt = model.stride, model.names, model.pt
 
 video_loader = cv2.VideoCapture(source)
 
-while True:
-    _, image = video_loader.read()
-    # s = np.stack([letterbox(x, img_sz, stride=stride, auto=True)[0].shape for x in image])
-    im = cv2.resize(image, img_sz, interpolation = cv2.INTER_AREA)
-    im = torch.from_numpy(im).to(device)
-    im = im.half() if half else im.float()
-    im /= 255.0 
-    im = torch.unsqueeze(im, 0)
-    im = torch.permute(im, (0, 3, 1, 2))
-    preds = model(im, augment=False, visualize=False)
-    results = non_max_suppression(preds, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+@smart_inference_mode()
+def run():
+    while True:
+        _, image = video_loader.read()
+        # s = np.stack([letterbox(x, img_sz, stride=stride, auto=True)[0].shape for x in image])
+        im = cv2.resize(image, img_sz, interpolation = cv2.INTER_AREA)
+        im = torch.from_numpy(im).to(device)
+        im = im.half() if half else im.float()
+        im /= 255.0 
+        im = torch.unsqueeze(im, 0)
+        im = torch.permute(im, (0, 3, 1, 2))
+        preds = model(im, augment=False, visualize=False)
+        results = non_max_suppression(preds, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
 
-    # Process detections
-    for i, det in enumerate(results):
+        # Process detections
+        for i, det in enumerate(results):
+            
+            # annotator for plotting
+            annotator = Annotator(image, line_width=2, example=str(names))
+
+            if det is not None and len(det): 
+                det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], image.shape).round()
+                for j, (output) in enumerate(det):
+                    bbox = output[0:4]
+                    conf = output[4]
+                    cls = output[5]
+                    annotator.box_label(bbox, f'{names[int(cls)]} {conf:.2f}', color=colors(int(cls), True))
+
+                # for deepsort
+                # outputs = deep_tracker.update(det.cpu(), ori_im)
+                # if len(outputs) > 0:
+                #     for j, (output) in enumerate(outputs):
+                #         bbox = output[0:4]
+                #         id = output[4]
+                #         cls = output[5]
+                #         conf = output[6]
+                #         annotator.box_label(bbox, f'{id} {names[int(cls)]} {conf:.2f}', color=colors(int(cls), True))
+
+
+        if show_video:
+            cv2.namedWindow("out", cv2.WINDOW_FREERATIO)
+            cv2.resizeWindow("out", image.shape[1], image.shape[0])
+            cv2.imshow("out", image)
+            if cv2.waitKey(1) == ord('q'):
+                exit()
         
-        # annotator for plotting
-        annotator = Annotator(image, line_width=2, example=str(names))
+        if save_video:
+            frame = cv2.resize(image, img_sz, interpolation=cv2.INTER_AREA)
+            out_writter.write(frame)
 
-        if det is not None and len(det): 
-            det[:, :4] = scale_boxes(im.shape[2:], det[:, :4], image.shape).round()
-            for j, (output) in enumerate(det):
-                bbox = output[0:4]
-                conf = output[4]
-                cls = output[5]
-                annotator.box_label(bbox, f'{names[int(cls)]} {conf:.2f}', color=colors(int(cls), True))
-
-            # for deepsort
-            # outputs = deep_tracker.update(det.cpu(), ori_im)
-            # if len(outputs) > 0:
-            #     for j, (output) in enumerate(outputs):
-            #         bbox = output[0:4]
-            #         id = output[4]
-            #         cls = output[5]
-            #         conf = output[6]
-            #         annotator.box_label(bbox, f'{id} {names[int(cls)]} {conf:.2f}', color=colors(int(cls), True))
-
-
-    if show_video:
-        cv2.namedWindow("out", cv2.WINDOW_FREERATIO)
-        cv2.resizeWindow("out", image.shape[1], image.shape[0])
-        cv2.imshow("out", image)
-        if cv2.waitKey(1) == ord('q'):
-            exit()
-    
-    if save_video:
-        frame = cv2.resize(image, img_sz, interpolation=cv2.INTER_AREA)
-        out_writter.write(frame)
+run()
 
 # # data loader
 # if webcam:
